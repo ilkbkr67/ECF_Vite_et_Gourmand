1dcff86fd37619830099bbeca9c39e3424bebc66
ilkbkr67 <ilkay.bakar@gmail.com>
2026-01-10 22:23:18 +0100

    Mise en place de la page d'accueil, ajustement du deuxième bloc necessaire

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
-import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
 import './stimulus_bootstrap.js';
+import 'bootstrap';
+import './styles/app.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
